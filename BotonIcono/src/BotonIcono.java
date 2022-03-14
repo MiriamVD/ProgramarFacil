6600c8eec4862f8616f78db53f658d560ec45f51
MiriamVD <101569945+MiriamVD@users.noreply.github.com>
2022-03-14 15:36:53 +0100

    Add files via upload

---
+++ BotonIcono/src/BotonIcono.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class BotonIcono  extends JButton implements Serializable{
+		
+		private static final long serialVersionUID = -6299481093603650156L;
+		ImageIcon icono;
+		
+		
+		BotonIcono(String texto, String path){
+			super();
+			this.setIcon(new ImageIcon(path));
+			this.setText(texto);
+			this.setSize(25,160);
+			this.setHorizontalAlignment(LEFT);
+			this.setVisible(true);
+		}
+	
+		public ImageIcon getIcono() {
+			return icono;
+		}
+		
+
+}
